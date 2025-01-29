18eface71d799f31c07bf39374c927686132c3ee
Ivonne Cantor <ivonnecp@gmail.com>
2025-01-29 12:27:02 +0100

    ejercicio 4 ok, falta el 3

---
+++ ejercicio04.php
@@ -0,0 +1,18 @@
+<?php
+    $my = array(
+        "Nombre" => "Ivonne",
+        "Edad" => "49",
+        "email" => "ivonnecp@gmail.com",
+        "Comida favorita" => "Giozas"
+    );
+
+    foreach ($my as $m => $y){
+        echo $m . ": " . $y . PHP_EOL;
+
+    }
+    
+      
+
+
+
+?>
